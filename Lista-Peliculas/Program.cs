c4f5f6135a31834de05de3fdcc77791230a3df74
EchoWitcher <54376199+EchoWitcher@users.noreply.github.com>
2019-11-20 15:31:02 -0800

    Lista de Peliculas

---
+++ Lista-Peliculas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lista_Peliculas
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> lista =new List<String>();
+                lista.Add("Megamente");
+                lista.Add("Planeta del tesoro");
+                lista.Add("Invader Zim (into the florpus)");
+                lista.Add("Now you see me");
+                lista.Add("El exorcista");
+
+           foreach(String Pelicula in lista)
+                Console.WriteLine(Pelicula);
+        }
+    }
+}
