fdcb8d84a9afd588b9a316d850bebfd496120e6b
EchoWitcher <54376199+EchoWitcher@users.noreply.github.com>
2019-11-20 15:04:30 -0800

    Herencia

---
+++ Herencia/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Herencia
+{
+    class Program
+    {
+    class Persona
+    {
+        protected string _nombre="";
+
+        public string Nombre 
+        {
+            get
+            {
+                return _nombre;
+            }
+        }
+
+        public Persona(string n)
+        {
+            _nombre = n ;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("Mi nombre es{0}",_nombre);
+        }
+
+    }
+    class Alumno : Persona
+    {
+        protected string num_control;
+        public Alumno(string nombre,string num_control):base(nombre)
+        {
+            this.num_control=num_control;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona tom = new Persona("Scott");
+            Persona jim = new Persona("Dwight");
+
+            tom.print();
+            jim.print();
+
+            Alumno luis = new Alumno("Clay","13195412");
+            luis.print();
+
+
+
+        }
+    }
+}}
+
