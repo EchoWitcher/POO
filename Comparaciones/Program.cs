39e88c86f9bbbec8dd0224e5ae03ab0083238c87
EchoWitcher <54376199+EchoWitcher@users.noreply.github.com>
2019-11-20 15:13:37 -0800

    Comparaciones

---
+++ Comparaciones/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Comparaciones
+{
+    
+    class Persona
+    {
+        int id;
+        string nombre;
+
+        public Persona (int id, string n)
+        {
+            this.id=id;
+            nombre= n;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1}", id, nombre);
+        }
+
+        public int CompareTo(Object o)
+        {
+            return this.id.CompareTo(((Persona)o).id);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> pers=new List<Persona>();
+            pers.Add(new Persona(2,"Roy"));
+            pers.Add(new Persona(6,"Jim"));
+            pers.Add(new Persona(5,"Pam"));
+
+            foreach(Persona p in pers)
+            Console.WriteLine(p);
+        }
+    }
+ }
